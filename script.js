7f7019e0a27267e834a7a5886257444dea411428
Adel <adel.alhadad333@gmail.com>
2022-03-06 05:37:48 +0200

    Do Some Changes at Review Section

--- script.js
+++ script.js
@@ -59,29 +59,29 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
   });
 
-  var swiper = new Swiper(".container", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-    loop:true,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-    },
-  });
+//   var swiper = new Swiper(".container", {
+//     spaceBetween: 20,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 7500,
+//       disableOnInteraction: false,
+//     },
+//     loop:true,
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         640: {
+//           slidesPerView: 2,
+//         },
+//         768: {
+//           slidesPerView: 2,
+//         },
+//         1024: {
+//           slidesPerView: 3,
+//         },
+//     },
+//   });
 
 
 // Add Loader Gif
